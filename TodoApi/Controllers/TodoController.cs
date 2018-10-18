22b04e1b922f4945c1faccd068b3cc2ddb1b9e50
Rob Martin <robert.martin@gmsl.co.uk>
2018-10-18 08:52:29 +0100

    Added GetAll() and GetById()

--- TodoApi/Controllers/TodoController.cs
+++ TodoApi/Controllers/TodoController.cs
@@ -23,5 +23,22 @@
                 _context.SaveChanges();
             }
         }
+
+        [HttpGet]
+        public ActionResult<List<TodoItem>> GetAll()
+        {
+            return _context.TodoItems.ToList();
+        }
+
+        [HttpGet("{id}", Name = "GetTodo")]
+        public ActionResult<TodoItem> GetById(long id)
+        {
+            var item = _context.TodoItems.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return item;
+        }
     }
 }
