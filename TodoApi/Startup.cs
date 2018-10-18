bca0ab5ec31f9e17e086a75a707cebd9442b7b33
Rob Martin <robert.martin@gmsl.co.uk>
2018-10-18 11:24:43 +0100

    Added CRUD Razor pages

--- TodoApi/Startup.cs
+++ TodoApi/Startup.cs
@@ -1,10 +1,13 @@
-﻿namespace TodoApi
+﻿
+
+namespace TodoApi
 {
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.DependencyInjection;
     using Models;
+    using Microsoft.AspNetCore.Hosting;
 
     public class Startup
     {
@@ -16,8 +19,22 @@
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
-        public void Configure(IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
             app.UseMvc();
         }
     }
